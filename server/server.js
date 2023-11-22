6609b84a88f8e99a371643a0a57090e20fa758dc
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-22 19:32:36 -0300

    ok

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post('/', async (req, res) => {
         model: "text-davinci-003",
         prompt: `${prompt}`,
         temperature: 0.5,
-        max_tokens: 200, 
+        max_tokens: 3000, 
         top_p: 0.4, 
         frequency_penalty: 0.5, 
         presence_penalty: 0, 
