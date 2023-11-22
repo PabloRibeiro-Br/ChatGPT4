9f417ba7cdfe4abae0e1ac4574b4b2cd5264536d
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-22 19:04:28 -0300

    client update

--- client/script.js
+++ client/script.js
@@ -63,54 +63,59 @@ function chatStripe(isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const data = new FormData(form)
-
+    e.preventDefault();
+  
+    const data = new FormData(form);
+  
     // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  
     // to clear the textarea input 
-    form.reset()
-
+    form.reset();
+  
     // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  
     // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
-
+  
     // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
-
+    const messageDiv = document.getElementById(uniqueId);
+  
     // messageDiv.innerHTML = "..."
-    loader(messageDiv)
-
-    const response = await fetch('https://gpt4-yagd.onrender.com', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
-
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
-
+    loader(messageDiv);
+  
+    const response = await fetch('https://gpt4-yagd.onrender.com', { // Altere para o endereço do seu servidor local
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    });
+  
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
+  
     if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-        typeText(messageDiv, parsedData)
+      const data = await response.json();
+      const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+  
+      if (parsedData.toLowerCase().includes('só respondo sobre cachorros')) {
+        // Exibir uma mensagem especial caso o assistente só responda sobre cachorros
+        typeText(messageDiv, "Desculpe, só respondo sobre cachorros.");
+      } else {
+        typeText(messageDiv, parsedData);
+      }
     } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
+      const err = await response.text();
+  
+      messageDiv.innerHTML = "Something went wrong";
+      alert(err);
     }
-}
+  };
 
 form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
@@ -118,3 +123,5 @@ form.addEventListener('keyup', (e) => {
         handleSubmit(e)
     }
 })
+
+
