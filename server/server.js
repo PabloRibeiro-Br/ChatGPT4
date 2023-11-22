5f7d21b34d6c1dbe63ffd68badbdbfa4cfcb3ffa
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-22 18:48:01 -0300

    update do chat

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
-
 dotenv.config()
 
 const configuration = new Configuration({
@@ -10,34 +9,41 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Olá, bem vindo!'
+    message: 'Olá, bem-vindo! Este é um assistente sobre cachorros.'
   })
 })
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const prompt = req.body.prompt.toLowerCase();
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0.5, // Higher values means the model will take more risks.
-      max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 0.4, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
+    // Verifique se o prompt está relacionado a cachorros
+    if (prompt.includes('cachorro') || prompt.includes('cão') || prompt.includes('animal de estimação')) {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0.5,
+        max_tokens: 200, 
+        top_p: 0.4, 
+        frequency_penalty: 0.5, 
+        presence_penalty: 0, 
+      });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
+      res.status(200).send({
+        bot: response.data.choices[0].text
+      });
+    } else {
+      res.status(200).send({
+        bot: 'Desculpe, este assistente responde apenas sobre cachorros.'
+      });
+    }
 
   } catch (error) {
     console.error(error)
